4c103ff2140e2019dea2fbd576b86b2382d28365
hik1tka <gaponkonikita7@gmail.com>
2026-07-16 19:54:24 +0300

    Нові ефекти (поки без толкової логіки)

---
+++ src/main/java/hik1tka/risen_races/effect/PurificationEffect.java
@@ -0,0 +1,10 @@
+package hik1tka.risen_races.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class PurificationEffect extends StatusEffect {
+    public PurificationEffect() {
+        super(StatusEffectCategory.NEUTRAL, 16766720);
+    }
+}
---
+++ src/main/java/hik1tka/risen_races/effect/ZombificationEffect.java
@@ -0,0 +1,33 @@
+package hik1tka.risen_races.effect;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+public class ZombificationEffect extends StatusEffect {
+    public ZombificationEffect() {
+        super(StatusEffectCategory.HARMFUL, 0x630B0B);
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 0, false, false, false));
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 100, 2, false, false, false));
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 100, 2, false, false, false));
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 0, false, false, false));
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return duration % 20 == 0; // Спрацьовує раз на секунду
+    }
+
+    @Override
+    public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+
+        super.onApplied(entity, attributes, amplifier);
+    }
+}
