651d59718ad5388780f3f83734e3f44e5d13a825
hik1tka <gaponkonikita7@gmail.com>
2026-07-16 20:00:26 +0300

    Додано до головного класу реєстратори

--- src/main/java/hik1tka/risen_races/RisenRaces.java
+++ src/main/java/hik1tka/risen_races/RisenRaces.java
@@ -1,5 +1,8 @@
 package hik1tka.risen_races;
 
+import hik1tka.risen_races.register.ModEffect;
+import hik1tka.risen_races.register.ModEntity;
+import hik1tka.risen_races.register.ModItem;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
@@ -11,8 +14,14 @@ public class RisenRaces implements ModInitializer {
 	public static final String MOD_ID = "risen_races";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    //Initialise registry
 	@Override
 	public void onInitialize() {
+        LOGGER.info("Mod has been initialised");
+
+        ModEffect.registerModEffect();
+        ModItem.registerModItem();
+        ModEntity.registryModEntity();
 	}
 
 	public static Identifier id(String path) {
