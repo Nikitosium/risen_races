2a55fea12ee841e7da49f50016e5a0446ac46b07
hik1tka <gaponkonikita7@gmail.com>
2026-07-16 20:00:40 +0300

    Клієнт-реєстратор

--- src/main/java/hik1tka/risen_races/client/RisenRacesClient.java
+++ src/main/java/hik1tka/risen_races/client/RisenRacesClient.java
@@ -1,11 +1,14 @@
 package hik1tka.risen_races.client;
 
+import hik1tka.risen_races.client.humanoid.human.HumanEntityRender;
+import hik1tka.risen_races.entity.humanoid.human.HumanEntity;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public class RisenRacesClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Тут буде твій код для ініціалізації клієнтської частини (наприклад, реєстрація рендерів)
+        EntityRendererRegistry.register(HumanEntity.HUMAN, HumanEntityRender::new);
     }
 }
